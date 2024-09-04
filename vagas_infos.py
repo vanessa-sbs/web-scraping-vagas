f72adeee69c1363c8019fcacf2bb3554e33993f5
Vanessa <vanessa_sbs@outlook.com>
2024-09-04 03:03:42 -0300

    --- Web Scraping vagas Catho ---
    - Automatização do clique do botao fechar anuncio
    - Automatização do clique do botão q aceita cookies
    - Criação da lista das vagas anunciadas na primeira pagina

--- vagas_infos.py
+++ vagas_infos.py
@@ -13,16 +13,23 @@ from time import sleep
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True) #necessario para navegador não abrir e fechar automaticamente
-chrome_options.add_argument('window-size=400,800') #Define o tamanho que vc quer do navegador
+chrome_options.add_argument('window-size=600,1000') #Define o tamanho que vc quer do navegador
 #chrome_options.add_argument('--headless')
 
 servico = Service(ChromeDriverManager().install())#para realizar uma atualização do chrome driver para acompanhar a versao do Chrome
 navegador = webdriver.Chrome(service=servico, options=chrome_options)
 
-navegador.implicitly_wait(10)
+navegador.implicitly_wait(15)
 
 navegador.get('https://www.catho.com.br/vagas/')
 
 btn_close_add = navegador.find_element(By.ID, 'closeTimedAd')
+btn_close_add.click()
 
-print(btn_close_add.text)
+btn_close_cookies = navegador.find_element(By.CLASS_NAME, "acceptAll")
+btn_close_cookies.click()
+#print(btn_close_add.text)'''
+
+vagas_lista = navegador.find_elements(By.CSS_SELECTOR, "article[id^='job-']")
+print(len(vagas_lista)) 
+
