f830a7c5a2f0140822e6c75ffcde2234160d93a1
Vanessa <vanessa_sbs@outlook.com>
2024-09-04 16:04:31 -0300

    --- Web Scraping vagas Catho ---
    - Localiza titulo da vaga
    - Clica no link de uma vaga na lista
    - Localiza descrição da vaga

--- vagas_infos.py
+++ vagas_infos.py
@@ -6,14 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 
 from time import sleep
 
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True) #necessario para navegador não abrir e fechar automaticamente
-chrome_options.add_argument('window-size=600,1000') #Define o tamanho que vc quer do navegador
+chrome_options.add_argument('window-size=800,1000') #Define o tamanho que vc quer do navegador
 #chrome_options.add_argument('--headless')
 
 servico = Service(ChromeDriverManager().install())#para realizar uma atualização do chrome driver para acompanhar a versao do Chrome
@@ -28,8 +27,26 @@ btn_close_add.click()
 
 btn_close_cookies = navegador.find_element(By.CLASS_NAME, "acceptAll")
 btn_close_cookies.click()
-#print(btn_close_add.text)'''
+#print(btn_close_add.text)''
 
+#Pega a primira lista de vagas
 vagas_lista = navegador.find_elements(By.CSS_SELECTOR, "article[id^='job-']")
 print(len(vagas_lista)) 
+
+
+####Encontra link da vaga e clica
+#(vagas_lista[0].get_attribute("outerHTML"))
+selector = "h2 > a"
+link_vaga = vagas_lista[0].find_element(By.CSS_SELECTOR, selector)
+#print(link_vaga.get_attribute("outerHTML"))
+##Título da vaga
+print(link_vaga.text)
+link_vaga.click()
+
+
+#Descrição da vaga
+descricao = navegador.find_element(By.CSS_SELECTOR, "#job-description")
+print(descricao.text)
+
+
 
