47b6486d76afc15aa4daaba15255a6f97d2fdc4c
Vanessa <vanessa_sbs@outlook.com>
2024-09-08 08:26:55 -0300

    --- Tags do Infojobs para o Beatifulsoup ---
    - Atualização das tags
    - Faltou terminar logica das exigengias/valorizados e beneficios

--- tags_infojobs_bs4.py
+++ tags_infojobs_bs4.py
@@ -21,24 +21,29 @@ empresa_element = soup.find('a', attrs={'target': '_blank'})
 empresa = empresa_element.text.strip() if empresa_element else 'Empresa confidencial'
 print(empresa)
 
-elementos1 = soup.findAll('div', class_='text-medium mb-4')
-local = elementos1[0].text.strip()
-salario = elementos1[1].text.strip()
+aux1_elements = soup.findAll('div', class_='text-medium mb-4')
+local = aux1_elements[0].text.strip()
+salario = aux1_elements[1].text.strip()
 print(salario)
 print(local)
 
-descricao_element = soup.find('p', attrs={'class': 'mb-16 text-break white-space-pre-line'})
-descricao = descricao_element.text.strip()
-#print(descricao)
+descricao_element = soup.findAll('p')#, attrs={'class': 'mb-16 text-break white-space-pre-line'})
+descricao = descricao_element[0].text.strip()
+contrato = descricao_element[2].text.strip()
+print(contrato)
 
-modelo_trabalho_element = soup.findAll('p')
-modelo_trabalho = modelo_trabalho_element[2].text
+modelo_trabalho_element = soup.find('div', class_= "text-medium small font-weight-bold mb-4")
+modelo_trabalho = modelo_trabalho_element.text.strip()
 print(modelo_trabalho)
 
-#Exigencias/ Valorizado/Beneficios
-elementos2 = soup.findAll('li')
-for x in elementos2:
-    print(x.text)
+#Exigencias/ Valorizado/ Beneficios
+aux = soup.findAll('div', class_ = "h4 font-weight-bold text-body mb-12")
+aux2 = soup.findAll('li')
+for i in (0, len(aux)-1):
+    if aux[i]:
+        print(aux[i].text.strip() + aux2[i].text.strip())
+    else:
+        print('nao encontrado')
 
 #Habilidades
 elementos3 = soup.findAll('div', class_ = 'tag mr-8 mb-8 tag-outline-primary tag-lg')
